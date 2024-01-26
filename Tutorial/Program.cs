cf8bdc770c8e6d8aa8820b8a27929a9135712cbe
EESSAMMB <EESSAMMB@S0158728>
2024-01-26 17:55:55 +0100

    Add project files.

---
+++ Tutorial/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
